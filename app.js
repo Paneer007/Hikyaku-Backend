200d96b32dc93220a5020936577ac00b42bac412
Sanjai <sanjaikumar.mani@gmail.com>
2022-09-28 20:48:23 +0530

    feat: added OAuth

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const signupRouter= require('./controller/signupRouter');
 const loginRouter= require('./controller/loginRouter');
 const userRouter = require('./controller/userRouter')
 const groupRouter = require('./controller/groupRouter')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 app.use(express.json())
+app.use(session({
+    secret:process.env.SESSIONTHINGY,
+    store:MongoStore.create({
+        mongoUrl:process.env.MONGO_URL
+    })
+}))
 try{
     mongoose.connect(process.env.MONGO_URL)
     console.log('no error') 
--- controller/loginRouter.js
+++ controller/loginRouter.js
@@ -3,7 +3,112 @@ const loginRouter= require("express").Router()
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const passport= require('passport')
-const GoogleStrategy = require('passport-google-oauth2')
+const { v4: uuidv4 } = require('uuid');
+const app = require("../app")
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const GitHubStrategy= require('passport-github2').Strategy;
+const TwitterStrategy= require('passport-twitter').Strategy;
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://localhost:3001/api/login/oauth2/redirect/twitter"
+  },
+  async(accessToken, refreshToken, profile, cb)=> {
+    try{
+        let user = await User.findOne({Name:profile.displayName})
+        if(!user){
+            const pass = uuidv4()
+            let saltRound = 10
+            const passHash = await bcrypt.hash(pass,saltRound)
+            user = new User({
+                Name:profile.displayName,
+                Password: passHash
+            })
+            await user.save()
+        }
+
+        const tokenBody = {id:user._id,username:user.Name}
+        const jwtToken = jwt.sign(tokenBody,process.env.SECRET)
+        return cb(null,jwtToken)
+    }
+    catch(e){
+        return cb(e,null)
+    }
+}));
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3001/api/login/oauth2/redirect/github"
+  },async(accessToken, refreshToken, profile, cb)=> {
+    try{
+        let user = await User.findOne({Name:profile.displayName})
+        if(!user){
+            const pass = uuidv4()
+            let saltRound = 10
+            const passHash = await bcrypt.hash(pass,saltRound)
+            user = new User({
+                Name:profile.displayName,
+                Password: passHash
+            })
+            await user.save()
+        }
+
+        const tokenBody = {id:user._id,username:user.Name}
+        const jwtToken = jwt.sign(tokenBody,process.env.SECRET)
+        return cb(null,jwtToken)
+    }
+    catch(e){
+        return cb(e,null)
+    }
+}));
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3001/api/login/oauth2/redirect/google"
+  },
+  async(accessToken, refreshToken, profile, cb)=> {
+    try{
+        let user = await User.findOne({Name:profile.displayName})
+        console.log("the user is",user)
+        if(!user){
+            const pass = uuidv4()
+            let saltRound = 10
+            const passHash = await bcrypt.hash(pass,saltRound)
+            user = new User({
+                Name:profile.displayName,
+                Password: passHash
+            })
+            await user.save()
+            console.log('status')
+        }
+
+        const tokenBody = {id:user._id,username:user.Name}
+        const jwtToken = jwt.sign(tokenBody,process.env.SECRET)
+        return cb(null,jwtToken)
+    }
+    catch(e){
+        return cb(e,null)
+    }
+  }
+));
+loginRouter.get('/oauth2/redirect/twitter',passport.authenticate('twitter'),async(req,res)=>{
+    const token={token:req.user}
+    return res.redirect(`http://localhost:3000?personalToken=${req.user}`)
+})
+loginRouter.get('/oauth2/redirect/github',passport.authenticate('github'),async(req,res)=>{
+    const token={token:req.user}
+    return res.redirect(`http://localhost:3000?personalToken=${req.user}`)
+})
+loginRouter.get('/oauth2/redirect/google',passport.authenticate('google') ,async(req,res)=>{
+    const token={token:req.user}
+    return res.redirect(`http://localhost:3000?personalToken=${req.user}`)
+})
 loginRouter.post('/',async(req,res)=>{
     console.log('hi')
     const body= req.body
@@ -22,6 +127,7 @@ loginRouter.post('/',async(req,res)=>{
     res.cookie('jwt',jwtToken,{httpOnly:true})
     return res.status(200).send({jwtToken:jwtToken})
 })
-loginRouter.get('/passport/google',passport.authenticate('google'))
-
+loginRouter.get('/federated/google',passport.authenticate('google',{ scope: ['profile', 'email'] }))
+loginRouter.get('/federated/github',passport.authenticate('github',{scope:['user:email']}))
+loginRouter.get('/federated/twitter',passport.authenticate('twitter'))
 module.exports = loginRouter
