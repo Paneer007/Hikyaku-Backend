c16fb7c3afbf5376afc2fdac3b083879a97945e4
Sanjai <sanjaikumar.mani@gmail.com>
2022-10-24 20:29:44 +0530

    started working on image uploader

--- controller/userRouter.js
+++ controller/userRouter.js
@@ -1,6 +1,7 @@
 const userRouter = require('express').Router()
 const User = require('../model/User')
 const validateCredentials= require('../middleware/validateCredentials')
+const { multerUploads } = require('../middleware/multer')
 
 userRouter.get('/',validateCredentials,async(req,res)=>{
     let id = res.locals.token.id
@@ -21,5 +22,7 @@ userRouter.post('/updateuser',validateCredentials,async(req,res)=>{
     await user.save()
     return res.status(200).send({message:"your mam"})
 })
+
+userRouter.post('/upload',multerUploads,validateCredentials,)
 
 module.exports = userRouter
